e679c516c2e09987eb0790f3bab4f2f573eba420
mkuschel <matteo.kuschel@gmx.de>
2022-11-09 08:31:11 +0100

    Implement "processSensorValues" and code structure

--- indoorgarden/indoorgarden.ino
+++ indoorgarden/indoorgarden.ino
@@ -1,9 +1,30 @@
+int inputTemperature = A0;
+int led1 = 2;
+float minTemp = 20;
+float inputvalue;
+
 void setup() {
-  // put your setup code here, to run once:
-
+  pinMode(inputTemperature, INPUT);
+  pinMode(led1, OUTPUT);
+  Serial.begin(9600);
 }
-
+void processSensorValues(int inputvalue, int output)
+{
+  if (inputvalue < minTemp) 
+  {
+    digitalWrite(output, HIGH);
+  } else
+  {
+    digitalWrite(output, LOW);
+  }
+  
+}
+float GetTemperature()
+{
+}
 void loop() {
-  // put your main code here, to run repeatedly:
+    inputvalue := GetTemperature();
+    processSensorValues(inputvalue, led1);
+
 
 }
