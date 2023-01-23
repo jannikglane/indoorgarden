a3d1af83eb692b850fa64e7b64192d603b7ad3f6
Jannik Glane <jannikglane@posteo.de>
2023-01-23 20:59:24 +0100

    fix: pin layout

--- indoorgarden/indoorgarden.ino
+++ indoorgarden/indoorgarden.ino
@@ -6,8 +6,8 @@ Date: 25.01.2023
 #include "menu.h"
 
 // configuring lcd display
-const int rs = 2, e = 4, d4 = 8, d5 = 9, d6 = 10, d7 = 11;
-LiquidCrystal lcd(rs, e, d4, d5, d6, d7);
+// const int rs = 2, e = 4, d4 = 8, d5 = 9, d6 = 10, d7 = 11;
+LiquidCrystal lcd(12, 11, 10, 9, 8, 7);
 
 // configuring buttons
 const int button_up = 5, button_down = 7, button_ok = 6; // button pins
@@ -78,5 +78,5 @@ void loop() {
   processOutput(&lcd);
 
   // prevents multiple reads of a button press
-  delay(50);
+  delay(150);
 }
