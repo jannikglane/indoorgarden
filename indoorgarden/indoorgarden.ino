80584362f87792bd4f18ab840e01673c913d2155
mkuschel <matteo.kuschel@gmx.de>
2023-01-23 19:23:21 +0100

    Merge from mkuschel

--- indoorgarden/indoorgarden.ino
+++ indoorgarden/indoorgarden.ino
@@ -1,125 +1,106 @@
-#include <LiquidCrystal.h>
+// include libraries
 #include <DHT.h>
-#define PIN 3
-#define PIN2 4
-#define TYPE DHT22
+#include <DHT_U.h>
+#include <Wire.h>
+#include <BH1750.h>
+
+// initialize BH1750 object
+BH1750 GY302;
+
+//initialize DHT object
+#define PIN 2
+#define TYPE DHT11
 DHT dht(PIN, TYPE);
-DHT dht2(PIN2, TYPE);
 
-////////////////////////////////////////////////////////////////////////////
-// configuring lcd display
-// LCD RS pin to digital pin 12
-// LCD Enable pin to digital pin 11
-// LCD D4 pin to digital pin 5
-// LCD D5 pin to digital pin 4
-// CD D6 pin to digital pin 3
-// LCD D7 pin to digital pin 2
-////////////////////////////////////////////////////////////////////////////
-const int rs = 2, e = 4, d4 = 8, d5 = 9, d6 = 10, d7 = 11;
-LiquidCrystal lcd(rs, e, d4, d5, d6, d7);
+// define min and max values
+float maxTemperature = 18;
+float maxHumidity = 50;
+float minSoilMoisturepercent = 40;
+float minLightLevel = 40;
 
-int led1 = 12;
-float maxTemperature = 20;
-float maxHumidity = 30;
+// define pins
+const int ventilator = 3;
+const int ledmatrix = 4;
+const int waterpump = 6;
+const int soilmoistureSensor = A0;
 
-////////////////////////////////////////////////////////////////////////////
-// Starting Initializing data of soilmoisture
-const int AirValue = 620;   // you need to replace this value with Value_1
-const int WaterValue = 310; // you need to replace this value with Value_2
-int soilMoistureValue = 0;
-int soilmoisturepercent = 0;
-// Ending Initializing data of soilmoisture
-////////////////////////////////////////////////////////////////////////////
-
-////////////////////////////////////////////////////////////////////////////
-// Starting Initializing data of light
-int light = 0;
-// Ending Initializing data of light
-////////////////////////////////////////////////////////////////////////////
+// define values for soil moisture
+const float AirValue = 620;
+const float WaterValue = 310;
+float soilMoistureValue = 0;
+float soilmoisturepercent = 0;
 
 void setup()
 {
-  pinMode(led1, OUTPUT);
+  // start dht library for temperature sensor
   dht.begin();
-  ////////////////////////////////////////////////////////////////////////////
-  // pinMode(led2, OUTPUT);
-  // dht2.begin();
-  ////////////////////////////////////////////////////////////////////////////
-  // initial lcd setup
-  lcd.begin(16, 2);
-  lcd.setCursor(0, 0);
-  lcd.print("IndoorGarden");
+
+  // start gy302 library for light sensor
+  GY302.begin();
+
+  // set pin mode
+  pinMode(ventilator, OUTPUT);
+  pinMode(ledmatrix, OUTPUT);
+  pinMode(waterpump, OUTPUT);
+  pinMode(soilmoistureSensor, INPUT);
 }
-void processTemperatureAndHumidity(float temperature, float humidity, int output, float maxTemperature, float maxHumidity)
+
+void processTemperatureAndHumidity()
 {
+  //read temperature and humidity
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+
+  // start ventilator if temperature or humidity is higher than max values
   if ((temperature > maxTemperature) || (humidity > maxHumidity))
   {
-    digitalWrite(output, HIGH);
+    digitalWrite(ventilator, LOW);
   }
   else
   {
-    digitalWrite(output, LOW);
+    //stop ventilator
+    digitalWrite(ventilator, HIGH);
   }
 }
 
 void processsoilMoisture()
 {
-  soilMoistureValue = analogRead(A0); // put Sensor insert into soil
-  Serial.println(soilMoistureValue);
+  // get sensor value 
+  soilMoistureValue = analogRead(soilmoistureSensor); 
+  // get soil moisture percent
   soilmoisturepercent = map(soilMoistureValue, AirValue, WaterValue, 0, 100);
-  if (soilmoisturepercent >= 100)
+
+  // start water pump if soilmoisturepercent is higher than minSoilMoisturepercent
+  if (soilmoisturepercent < minSoilMoisturepercent)
   {
-    Serial.println("100 %");
-    lcd.setCursor(0, 0);
-    lcd.print("Soil Moisture");
-    lcd.setCursor(0, 1);
-    lcd.print("100 %");
-    delay(250);
-    lcd.clear();
+    digitalWrite(waterpump, LOW);
   }
-  else if (soilmoisturepercent <= 0)
+  else
   {
-    Serial.println("0 %");
-    lcd.setCursor(0, 0);
-    lcd.print("Soil Moisture");
-    lcd.setCursor(0, 1);
-    lcd.print("0 %");
-    delay(250);
-    lcd.clear();
-  }
-  else if (soilmoisturepercent > 0 && soilmoisturepercent < 100)
-  {
-    Serial.print(soilmoisturepercent);
-    Serial.println("%");
-    lcd.setCursor(0, 0);
-    lcd.print("Soil Moisture");
-    lcd.setCursor(0, 1);
-    lcd.print(soilmoisturepercent);
-    lcd.print(" %");
-    delay(250);
-    lcd.clear();
+    //stop water pump
+    digitalWrite(waterpump, HIGH);
   }
 }
 
-void processLight()
+void processLightLevel()
 {
-  int raw_light = analogRead(light_sensor);    // read the raw value from light_sensor pin (A3)
-  int light = map(raw_light, 0, 1023, 0, 100); // map the value from 0, 1023 to 0, 100
-
-  Serial.print("Light level: ");
-  Serial.println(light); // print the light value in Serial Monitor
+  // get reading from gy302 light sensor
+  uint16_t lux = GY302.readLightLevel();
+  
+  //start ledmatrix if lightlevel is higher than minLightLevel
+  if (lux < minLightLevel)
+  {
+    digitalWrite(ledmatrix, HIGH);
+  }
+  else
+  {
+    digitalWrite(ledmatrix, LOW);
+  }
 }
 
 void loop()
 {
-  float temperature = dht.readTemperature();
-  float humidity = dht.readHumidity();
-  processTemperatureAndHumidity(temperature, humidity, led1, maxTemperature, maxHumidity);
-
-  // float soilMoistureValue = dht2.readHumidity();
-  // processsoilMoisture(soilMoistureValue,);
-  // TODO: Bodenfeuchtigkeit verarbeiten
+  processTemperatureAndHumidity();
   processsoilMoisture();
-  // TODO: Licht verarbeiten
-  processLight();
+  processLightLevel();
 }
